47542ae6cdcee8d67b3f03712303882470a58542
SONOE385 <kotarou385@gmail.com>
2022-04-15 13:00:21 +0900

    ホーム画面作成

--- resources/views/dinner.blade.php
+++ resources/views/dinner.blade.php
@@ -1,11 +1,11 @@
+@extends('layouts.app')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <title>献立一覧</title>
 </head>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,10 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/user-infomation.css') }}" rel="stylesheet">
+
+
 </head>
 <body>
     <div id="app">
