aeb9c1d1dc91e515ec9aacdff50471703762abf6
SONOE385 <kotarou385@gmail.com>
2022-04-20 11:34:39 +0900

    献立登録・編集画面等　追加

---
+++ resources/views/create_menu.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="body">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="user-infomation-contents">
+                <form method="POST" action="">
+                    @csrf
+                    <div class="user-infomation-title">
+                    献立登録
+                    </div>
+
+                    <div class="">
+                        <div class="row mb-3">              
+                            <div class="">
+                                <input id="group" list="dinner-group" type="text" class="form-control @error('group') is-invalid @enderror" name="group" value="{{ old('group') }}" placeholder="group" required autocomplete="group" autofocus>
+                                <datalist id="dinner-group">
+                                    <option value="Aグループ">
+                                    <option value="Bグループ">
+                                    <option value="Cグループ">
+                                </datalist>
+
+                                @error('group')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="row mb-3">                        
+                            <div class="">
+                                <input action="" id="main" type="text" class="form-control @error('main') is-invalid @enderror" name="main" value="{{ old('main') }}" placeholder="main" required autocomplete="main" autofocus>
+                                @error('main')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="row mb-3">                        
+                            <div class="">
+                                <input id="side" type="side" class="form-control @error('side') is-invalid @enderror" name="side" value="{{ old('side') }}" placeholder="side" required autocomplete="side">
+                                @error('side')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="row mb-3">                        
+                            <div class="">
+                                <input id="soup" type="soup" class="form-control @error('soup') is-invalid @enderror" name="soup" placeholder="soup" required autocomplete="new-soup">
+                                
+                                @error('soup')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="">
+                            <div class="">
+                                <button type="submit" class="btn btn-dark user-infomation-button">
+                                    {{ __('登録') }}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/group_list.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+<nav class="navbar text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="#page-top">Aグループ</a>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="">ホームへ戻る</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="body">
+    <div class="group-list">
+        <div class="group-list-box">
+            <div class="list-area">
+                <p class="text">
+                    main-------ハンバーグ</br>
+                    side-------サラダ</br>
+                    soup-------コンソメスープ           
+                </p>
+            </div>
+            <div class="list-area">
+                <p class="text">
+                    main-------ハンバーグ</br>
+                    side-------サラダ</br>
+                    soup-------コンソメスープ           
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
